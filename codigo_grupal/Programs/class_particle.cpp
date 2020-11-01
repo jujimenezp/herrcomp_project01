86bce9c49cd377db0ee07509d52f989962b0a309
Andres Vargas <anvargasl@unal.edu.co>
2020-11-01 00:00:25 -0500

    funcion move_hole y arreglo de funcion move para eliminar time_step

--- codigo_grupal/Programs/class_particle.cpp
+++ codigo_grupal/Programs/class_particle.cpp
@@ -8,12 +8,36 @@ int Particle::Getcell(const CONFIG &config){
     return X + Y*config.gridsize;
 }
 
-void Particle::Move(int step, int direction, const CONFIG &config){
+void Particle::Move(const int &step, const int &direction, const CONFIG &config, std::vector<int> &Cells)
+{
+  if (labs(position[direction] + step) != config.latticesize/2 + (1 - step)/2){
+    int m = Particles[random_particle].Getcell(config);
+    Cells[m] -= 1; //delete it from the old cell
+    
+    position[direction] += step; //move the particle
+    
+    m = Particles[random_particle].Getcell(config);
+    Cells[m] += 1; //add it to the new cell
+  } 
+}
 
-    if (labs(position[direction] + step) != config.latticesize/2 + (1 - step)/2){
-
-        position[direction] += step;
-
-    }
-
+void Particle::Move_hole(const int &time, const int &step, const int &direction, const int &particle_id, const CONFIG &config, std::vector<int> &Cells, std::vector<Particle> &Particles)
+{
+  int moved_particle_cell = Particles[particle_id].Getcell(config);
+  
+  if (labs(position[direction] + step) != config.latticesize/2 + (1 - step)/2){ //the particle is not on the limit after moving
+    Cells[moved_particle_cell] -= 1; //delete it form old-cell count
+    
+    position[direction] += step; //move the particle
+    
+    moved_particle_cell = Particles[particle_id].Getcell(config);
+    Cells[moved_particle_cell] += 1; //add it to new-cell count
+  }
+  else if (direction + step == config.holeposition && labs(position[(direction+1)%2]) <= config.holesize/2){ //makes sure the particle moved towards the hole and that it can get out
+    Cells[moved_particle_cell] -= 1; //delete the particle from the cell count
+    Particles[particle_id] = Particles[Particles.size() - 1];
+    Particles.pop_back(); //delete the particle
+    std::cout << time << "\t" << Particles.size() << std::endl;
+  }
+   
 }
